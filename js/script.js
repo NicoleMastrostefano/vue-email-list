127343f56b67acf2130540fe83b15765b866ac55
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-16 17:09:20 +0100

    html/js/css

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var app = new Vue ({
+    el:"#root",
+    data: {
+      emails:[]
+    },
+
+    mounted:function(){
+    for(var i = 0; i < 10; i++)
+    axios
+      .get("https:flynn.boolean.careers/exercises/api/random/mail")
+      .then((result)=>this.emails.push(result.data.response));
+    },
+})
