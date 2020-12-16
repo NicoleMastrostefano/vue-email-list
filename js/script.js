0d91e28353cce1b82770d1b28ac85554352dce7f
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-16 23:20:26 +0100

    v-if added

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ var app = new Vue ({
     },
 
     mounted:function(){
-    for(var i = 0; i < 10; i++)
-    axios
-      .get("https:flynn.boolean.careers/exercises/api/random/mail")
-      .then((result)=>this.emails.push(result.data.response));
+      for(var i = 0; i < 10; i++)
+      axios
+        .get("https:flynn.boolean.careers/exercises/api/random/mail")
+        .then((result)=>this.emails.push(result.data.response));
     },
 })
